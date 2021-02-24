0e85ef27ab88806d989eb303a741db204de67a03
Prabhav Agrawal <prabhavagrawal7@gmail.com>
2021-02-24 15:03:30 +0530

    a

--- file.py
+++ file.py
@@ -1,2 +1,20 @@
+m, n = 0,1
 
-print(len(input().split()))
+
+sum = 0
+def cases(mnew=1, nnew=1, memo={}):
+    
+    if((mnew, nnew) in memo):
+        return memo[(mnew, nnew)]
+
+    
+    if(mnew == m or nnew == n):
+        memo[(mnew, nnew)] =  1
+    
+        
+    else:
+        memo[(mnew, nnew)] = cases(mnew+1, nnew) + cases(mnew, nnew+1)
+    return memo[(mnew, nnew)]
+
+
+print(cases())
---
+++ neverendingrivalry.cpp
@@ -0,0 +1,29 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// // int minstring(string s){
+// //     int length = INT_MAX;
+// //     for (int i = 0; i < s.length(); i++)
+// //     {
+// //         if(s[i] != 0){
+            
+// //         }
+// //     }
+    
+// // }
+// int func()
+// {
+//     cout << small1;
+    
+//     return 0;
+// }
+// int main()
+// {
+//     //write your code from here
+//     int testcases;
+//     cin >> testcases;
+//     while (testcases--)
+//     {
+//         func();
+//     }
+//     return 0;
+// }
